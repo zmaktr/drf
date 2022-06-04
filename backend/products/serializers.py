9479eb60adba5d5472f49a768ebc6cc4ac1ad28a
Zaeem Akhtar <zmaktr@gmail.com>
2022-06-04 22:31:38 +0500

    add urls field in object using serializers.py

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from . models import Products
 
 
 class ProductsSerializer(serializers.ModelSerializer):
-    eid_price = serializers.SerializerMethodField(read_only=True)
+    eid_price   = serializers.SerializerMethodField(read_only=True)
+    url         = serializers.SerializerMethodField(read_only=True)
+    edit_url    = serializers.SerializerMethodField(read_only=True)
+    url_link      = serializers.HyperlinkedIdentityField(view_name='product_detail', lookup_field='pk') # HyperlinkedIdentityField works only with ModelSerializer otherwise use to above method
     class Meta:
         model   = Products
         fields  = [
+            'edit_url',
+            'url',
+            'url_link',
             'pk',
             'title', 
             'content', 
@@ -30,3 +37,17 @@ class ProductsSerializer(serializers.ModelSerializer):
             return None
         return obj.holiday_price()
 
+    def get_url(self, obj):
+        # return f"/api/products/{obj.pk}/"
+        request = self.context.get('request')
+        print(request)
+        if request is None:
+            return None
+        return reverse("product_detail", kwargs={"pk": obj.pk},  request=request)
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        print(request)
+        if request is None:
+            return None
+        return reverse("product_edit", kwargs={"pk": obj.pk},  request=request)
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,19 +4,19 @@ from . import views
 urlpatterns =[
 
     # class based views 
-    path('', views.ProductsListCreateAPIView.as_view()),
-    path('<int:pk>', views.ProductsDetailAPIView.as_view()),    
-    path('<int:pk>/update/', views.ProductsUpdateAPIView.as_view()),
+    path('', views.ProductsListCreateAPIView.as_view(), name="product_list"),
+    path('<int:pk>/', views.ProductsDetailAPIView.as_view(), name="product_detail"),    
+    path('<int:pk>/update/', views.ProductsUpdateAPIView.as_view(), name="product_edit"),
     path('<int:pk>/delete/', views.ProductsDeleteAPIView.as_view()),
     
     # mixins views 
     path('mixinlist/', views.ProductsMixinView.as_view()),
-    path('mixindetail/<int:pk>', views.ProductsMixinView.as_view()),
+    path('mixindetail/<int:pk>/', views.ProductsMixinView.as_view()),
     path('mixincreate/', views.ProductsMixinView.as_view()),
-    path('mixindelete/<int:pk>', views.ProductsMixinView.as_view()),
-    path('mixinupdate/<int:pk>', views.ProductsMixinView.as_view()),
+    path('mixindelete/<int:pk>/', views.ProductsMixinView.as_view()),
+    path('mixinupdate/<int:pk>/', views.ProductsMixinView.as_view()),
 
     # function based views 
-    path('<int:pk>', views.products_alt_view),
+    path('<int:pk>/', views.products_alt_view),
     path('', views.products_alt_view),
 ]
